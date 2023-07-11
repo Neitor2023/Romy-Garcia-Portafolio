3d2e85c0347bdbbc09038845ca15d1465fc64c54
Romy Garcia <neitorme@gmail.com>
2023-07-11 15:59:43 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing Animation
+var typed = new Typed(".typing",{
+    strings:["","Higher University Technician in Computer Science","Front-End","Back-End","Full Stack Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
