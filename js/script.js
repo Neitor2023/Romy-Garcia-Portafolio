164327176a31676fb2d9f1f8482fbb684b216b36
Romy Garcia <neitorme@gmail.com>
2023-07-14 17:44:44 -0500

    aplicando recomendaciones

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing Animation
 var typed = new Typed(".typing",{
-    strings:["","Higher University Technician in Computer Science","Front-End","Back-End","Full Stack Web Developer"],
+    strings:["","Técnico Superior Universitario en Informática.","Front-End","Back-End","Desarrollador Web Full Stack","Entusiasta apasionado de la tecnología + aprendizaje rápido + soluciones impactantes = ¡ mi valor excepcional !"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
